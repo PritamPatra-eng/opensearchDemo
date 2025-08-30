1ea6ec62be8ac4f0e4ffa9aa270b6b5de1d02ef9
Pritam Patra <patrapritam95@gmail.com>
2025-08-30 15:50:22 +0530

    master commit

---
+++ src/test/java/com/practice/opensearchdemo/OpensearchdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.opensearchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensearchdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
